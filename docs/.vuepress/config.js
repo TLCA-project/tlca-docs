1205df878fd24970ad38fe50217e27103cc68e92
Sébastien Combéfis <sebastien@combefis.be>
2022-06-18 18:33:24 +0200

    Initialise TLCA docs website

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'TLCA Documentation',
+      description: 'Documentation related to the TLCA project',
+    },
+    '/fr/': {
+      lang: 'fr-BE',
+      title: 'Documentation TLCA',
+      description: 'Documentation en lien avec le projet TLCA',
+    },
+  },
+  themeConfig: {
+    locales: {
+      '/': {
+        selectText: 'Languages',
+        label: 'English',
+        ariaLabel: 'Languages',
+        nav: [
+          { text: 'User guide', link: '/user/' },
+          { text: 'Developer guide', link: '/developer/' },
+        ],
+      },
+      '/fr/': {
+        selectText: 'Langues',
+        label: 'Français',
+        ariaLabel: 'Langues',
+        nav: [
+          { text: 'Guide de l\'utilisateur', link: '/fr/user/' },
+          { text: 'Guide du développeur', link: '/fr/developer/' },
+        ],
+      },
+    },
+  },
+}
